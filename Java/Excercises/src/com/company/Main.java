503b651001c0d52245127b28ab3f891a0382ed3f
KristiyanVasilev <make_05@abv.bg>
2017-04-20 00:10:17 +0300

    Added Java stuff

---
+++ Java/Excercises/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String[] firstArray = scan.nextLine().split("\\s");
+		String[] secondArray = scan.nextLine().split("\\s");
+
+	}
+}
